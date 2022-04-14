50224cbd91cf67765461bf86d20975f980f5065e
Raúl González <gonzalva@ciencias.unam.mx>
2022-04-14 17:43:01 -0500

    Agrega tests para code_decode, io, y lsb

---
+++ tests/codificador_decodificador/test_code_decode.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+
+from src.pkg.codificador_decodificador.code_decode import *
+
+class TestCodeDecode(TestCase):
+    def test_get_bits(self):
+        mensaje = "AH SI SOY"
+        esperado = "00000001111101010010010001101010010011101100011011"
+        obtenido = get_bits(mensaje)
+        self.assertEqual(obtenido, esperado)
+
+    def test_mensaje(self):
+        bits = "00000001111101010010010001101010010011101100011011"
+        esperado = "AH SI SOY"
+        obtenido = get_mensaje(bits)
+        self.assertEqual(obtenido, esperado)
+
+    def valida_tamanio(self):
+        mensaje = "AH SI SOY"
+        limite_valido = 50
+        obtenido = valida_tamanio(mensaje, limite_valido)
+        self.assertTrue(obtenido)
+        limite_no_valido = 45
+        obtenido = valida_tamanio(mensaje, limite_no_valido)
+        self.assertFalse(obtenido)
---
+++ tests/lsb/test_lsb.py
@@ -0,0 +1,34 @@
+from unittest import TestCase
+import numpy as np
+
+from src.pkg.lsb.lsb import *
+
+class TestLSB(TestCase):
+    def setUp(self):
+        self.pixeles = np.array(
+                [  [[186, 157,   2],
+                    [ 83, 152, 117],
+                    [ 29, 119, 158]],
+
+                   [[232, 222,  10],
+                    [236,  79,  80],
+                    [159, 131, 196]]])
+        self.bits = "1001011010011"
+        self.pixeles_modificados = np.array(
+                [  [[187, 156,   2],
+                    [ 83, 152, 117],
+                    [ 29, 118, 159]],
+
+                   [[232, 222,  11],
+                    [237,  79,  80],
+                    [159, 131, 196]]])
+        self.bits_extraidos = "100101101001110110"
+
+    def test_guarda_bits(self):
+        obtenido = guarda_bits(self.bits, self.pixeles)
+        resultado = np.testing.assert_array_equal(self.pixeles_modificados, obtenido)
+        self.assertIsNone(resultado)
+
+    def test_extrae_bits(self):
+        obtenido = extrae_bits(self.pixeles_modificados)
+        self.assertEqual(self.bits_extraidos, obtenido)
