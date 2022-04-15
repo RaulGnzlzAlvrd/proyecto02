34b6c542236a53c6d469ada7e4db1d694014e694
RebecaNovella <rebecanovella@ciencias.unam.mx>
2022-04-14 20:26:34 -0500

    Añade funciones get_bits y get_mensaje

--- src/pkg/codificador_decodificador/code_decode.py
+++ src/pkg/codificador_decodificador/code_decode.py
@@ -1,10 +1,30 @@
+import numpy as np 
+
+
+def llena_codigo():
+    codigo = {}
+    alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ /"
+    numero = 0
+    for letra in alfabeto:
+        codigo[letra] = np.binary_repr(numero,5)
+        numero += 1
+    return codigo    
+
+
+
 def get_bits(mensaje):
     """
     mensaje: string
 
     return string
     """
-    pass
+    bits = ""
+    codigo = llena_codigo()
+    for letra in mensaje:
+        bits += codigo[letra]
+    
+    return bits + "11011"
+
 
 def get_mensaje(bits):
     """
@@ -16,7 +36,22 @@ def get_mensaje(bits):
 
     return string
     """
-    pass
+    contador = 1
+    cadena = ""
+    mensaje = ""
+    volteado = voltea_codigo(llena_codigo())
+    bits2 = bits[:-5]
+    for bit in bits2: 
+        cadena += bit
+        if contador % 5 == 0:
+            mensaje += volteado[cadena]
+            cadena = ""
+        contador += 1
+    return mensaje 
+
+def voltea_codigo(codigo):
+    volteado = dict(zip(codigo.values(), codigo.keys()))
+    return volteado
 
 def valida_tamanio(mensaje, limite):
     """
